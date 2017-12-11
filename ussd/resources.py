e96d0b08660b94372c08d8a091e99f0e258df923
morfat <morfatmosoti@gmail.com>
2017-12-11 04:11:54 +0300

    implement pagination

--- ussd/resources.py
+++ ussd/resources.py
@@ -1,6 +1,7 @@
 import falcon
 import requests
 import pickle
+import operator
 
 
 def display_response(response,choice_error_message=None,response_data_len=None):
@@ -44,6 +45,13 @@ def make_http_request(url,params,session):
                 
 
 class UssdResource:
+    MAX_SCREEN_CHARS=90-30 #Maximu screen dispplay in chars.. 30 chars   #for navigation of 97,98 and 99.
+
+    def call_client_url(self,url,data):
+        response=requests.post(url=url,json=data)
+        return response.json()
+
+
     def get_ussd_input(self,operator_ussd_string):
         ussd_input=None
         if operator_ussd_string:
@@ -56,17 +64,99 @@ class UssdResource:
                 ussd_input=ussd_input_list[-1]
         return ussd_input
 
+    def paginate_client_response(self,response_data,ussd_input):
+        #@TODO implement ordering for response .. here before modifying
+        menus=response_data.get("menus")
+        menus.sort(key=operator.itemgetter("id")) #sort menus by id
+        message=response_data.get("message")
+
+        message_len=len(message) #message is applied on first menu only
         
 
+        screens=1
+        new_response_data={"menus":response_data.get("menus"),"screens":1,"url":response_data.get("url"),
+                          "client_session":response_data.get("client_session"),"current_screen":1}
+
+
+        available_chars=self.MAX_SCREEN_CHARS-message_len
+        
+       
+        screen=''
+        for m in menus:
+            menu=''
+            
+            if m.get('choice')=='0':
+               menu="\n%s"%(m.get('name'))
+            else:
+                menu="\n%s: %s"%(m.get('choice'),m.get('name'))
+
+            #check menu length
+            available_chars=available_chars-len(menu)
+            if available_chars>0:
+                if len(screen)==0:
+                    #inital screen . add heading
+                    screen+=message+menu
+                else:
+                    screen+=menu
+            else:
+                #we need newer page
+                screen=menu
+                available_chars=self.MAX_SCREEN_CHARS-message_len
+                screens=screens+1
+            
+            new_response_data.update({"screen_%s"%(screens):screen,"screens":screens})
+        
+        print (new_response_data)
+
+        return new_response_data
+
+
+
     def on_get(self,req,resp):
+        """ Sample Required Response from client : 
+          #id is needed for ordering. display is made in order of id ASC.
+
+                    {"menus":[{"id":1,choice":"1","name":"Manchester Vs Arsenal","info":{"game_id":"12345"}},
+                              {"id":2,choice":"2","name":"Chelsea Vs Magma","info":{}},
+                              {"id":3,choice":"3","name":"Orlando Vs Kelio","info":{}},
+                             ],
+                    "message":"CON Next 5 Games",
+                    "client_session":{},
+                    "url":"http://127.0.0.1:9000/footballFixture"
+                     }
+
+        """
         #we can access self.redis,self.service_endpoint,self.service_session_key,self.service_sub_code in this class
 
-        msisdn=req.params.get('msisdn')
-        operator_session_id=req.params.get('session_id')
         #get input
         ussd_input=self.get_ussd_input(req.params.get('ussd_string'))
+        #print (ussd_input)
+        
+        #get session object
+        session=self.redis.get(self.service_session_key)
 
-        print (ussd_input)
+        #default request data
+        request_data={'operator_session_id':req.params.get('session_id'),'msisdn':req.params.get('msisdn'),
+                      'ussd_input':ussd_input,
+                      "info":{}, #this is as from client. we return as per request
+                      "client_session":{} #this is from  client session. we return as per request
+                      }
+
+        if not session:
+            #new request, lets call external API.
+            response_data=self.call_client_url(url=self.service_endpoint,data=request_data)
+
+            #paginate response to create sample 
+
+            paginated_data=self.paginate_client_response(response_data,ussd_input)
+           
+
+            resp.media=paginated_data
+
+
+
+
+
 
 
                 
